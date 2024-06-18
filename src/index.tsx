178c94b4e3280a0ba3d485ccb4c3d43719c4286b
Alberto Gonzalez <alberto.gonzalez@softonic.com>
2024-06-18 13:04:27 +0100

    feat: server actions with root update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,35 @@
-import { Suspense, startTransition, use, type ReactNode } from 'react';
+import {
+  Suspense,
+  startTransition,
+  use,
+  type ReactNode,
+  useEffect,
+  useState,
+  useTransition,
+} from 'react';
 import { createRoot } from 'react-dom/client';
 import { createFromFetch, encodeReply } from 'react-server-dom-webpack/client';
+
+function generateKey() {
+  return Date.now().toString(36) + Math.random().toString(36).slice(2);
+}
+
+let updateContentKey = (key) => {
+  console.error('updateContentKey called before it was set!', key);
+};
+
+function onStreamFinished(fetchPromise, onFinished) {
+  // create a promise chain that resolves when the stream is completely consumed
+  return (
+    fetchPromise
+      // clone the response so createFromFetch can use it (otherwise we lock the reader)
+      // and wait for the text to be consumed so we know the stream is finished
+      .then((response) => response.clone().text())
+      .then(onFinished)
+  );
+}
+
+let contentPromise = createFromFetch(fetch('/rsc'), { callServer });
 
 async function callServer(id, args) {
   const fetchPromise = fetch('/action', {
@@ -8,14 +37,21 @@ async function callServer(id, args) {
     headers: { 'rsc-action': id },
     body: await encodeReply(args),
   });
-  const actionResponsePromise = createFromFetch(fetchPromise);
-  const { returnValue } = await actionResponsePromise;
+  const contentKey = generateKey();
+  onStreamFinished(fetchPromise, () => {
+    updateContentKey(contentKey);
+  });
+  contentPromise = createFromFetch(fetchPromise, { callServer });
+  const { returnValue } = await contentPromise;
   return returnValue;
 }
 
-const contentPromise = createFromFetch(fetch('/rsc'), { callServer });
-
 function Root() {
+  const [, setContentKey] = useState(null);
+  const [, startTransition] = useTransition();
+  useEffect(() => {
+    updateContentKey = (key) => startTransition(() => setContentKey(key));
+  }, []);
   const content = use<{ root: ReactNode }>(contentPromise).root;
   return content;
 }
